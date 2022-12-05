8537072f01405ee9ade4d555f35b8aa9b1389a49
Bogruk <bogruk44@gmail.com>
2022-12-05 08:22:17 +0200

    5 zadanie

--- Lab4/Dod_2.py
+++ Lab4/Dod_2.py
@@ -1,10 +1,11 @@
 import math
-x = [[3, 4 ,5],
+
+x = [[3, 4, 5],
      []]
 y = []
-y = y+x[0]
-for i in range(y):
-     math.pow(y[i], 2)
-     print(y)
-#x[1] = y
-print(p)
+y = y + x[0]
+for i in range(x[0]):
+    math.pow(x[i], 2)
+    print(y)
+# x[1] = y
+print(x)
